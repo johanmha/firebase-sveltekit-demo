05c40b0f3736cc2be4999fe9995c60975f795f37
Johan Haugland <johanhaugl@gmail.com>
2023-11-02 15:42:11 +0100

    firebase init

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+    apiKey: 'AIzaSyD8s7nyrBoJO65meVYEwACUO4N-TK8F6mk',
+    authDomain: 'fir-sveltekit-demo.firebaseapp.com',
+    projectId: 'fir-sveltekit-demo',
+    storageBucket: 'fir-sveltekit-demo.appspot.com',
+    messagingSenderId: '900769655376',
+    appId: '1:900769655376:web:74a7eab75c4d6d3c7bff55',
+};
